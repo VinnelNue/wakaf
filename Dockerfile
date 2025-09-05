c90b7c6788cab6137050429c4f39f4c723147a69
Nama Kamukevin <emailgithub@example.comkevinimmanuel812@gmail.com>
2025-09-06 03:29:40 +0700

    update index.php and conn.php and Dockerfile

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,14 @@
 FROM php:8.2-apache
 
+# Install ekstensi MySQL
 RUN docker-php-ext-install mysqli pdo pdo_mysql
 
+# Copy semua file ke /var/www/html
 COPY . /var/www/html/
-# Tambahkan healthcheck
-HEALTHCHECK --interval=10s --timeout=5s --start-period=5s --retries=3 \
-  CMD curl -f http://localhost/healthcheck.php || exit 1
 
-  
+# Healthcheck
+HEALTHCHECK --interval=10s --timeout=5s --start-period=5s --retries=3 \
+CMD curl -f http://localhost/healthcheck.php || exit 1
+
 EXPOSE 80
 CMD ["apache2-foreground"]
--- lib/conn.php
+++ lib/conn.php
@@ -1,27 +1,11 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
-
-if (file_exists(__DIR__ . '/.env')) {
-    $lines = file(__DIR__ . '/.env', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
-    foreach ($lines as $line) {
-        if (strpos(trim($line), '#') === 0) continue;
-        list($name, $value) = explode('=', $line, 2);
-        putenv(sprintf('%s=%s', $name, $value));
-    }
-}
-
-// Ambil env vars
 $db_host = getenv('DB_HOST');
 $db_user = getenv('DB_USER');
 $db_pass = getenv('DB_PASS');
 $db_name = getenv('DB_NAME');
 
-// Koneksi MySQL
 $conn = new mysqli($db_host, $db_user, $db_pass, $db_name);
 
-// Cek koneksi
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
